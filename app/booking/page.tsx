7f948bcab6fbb835ee640794d557a2489a7c4f61
marvellous <marvellousolorunwa23@gmail.com>
2026-07-20 10:01:07 +0100

    Changed the styling for the form

--- app/booking/page.tsx
+++ app/booking/page.tsx
@@ -4,12 +4,12 @@ import Form from '@/components/Form';
 import {motion} from "framer-motion"
 
 export default function SomePage() {
-  const [isFormOpen, setIsFormOpen] = useState(false);
+  // const [isFormOpen, setIsFormOpen] = useState(false);
 
   return (
     <div className="w-full bg-black py-8 md:py-20 pt-22 md:pt-32 px-3 md:px-6">
       {/* whatever triggers the form, e.g. a button */}
-      {isFormOpen === false && (
+      {/* {isFormOpen === false && (
         <motion.button
           animate={{
             y: [-3, 3, 0, -3, 3],
@@ -24,12 +24,12 @@ export default function SomePage() {
         className="flex items-center justify-center bg-[#FE5A7A] text-white px-6 py-3 rounded-full text-base md:text-lg leading-[160%] hover:opacity-90 transition-opacity w-[200px] h-[53px] mt-20 mx-auto">
           Open Form
         </motion.button>
-      )}
+      )} */}
 
 
-      <Activity mode={isFormOpen === true ? 'visible' : 'hidden'}>
+      {/* <Activity mode={isFormOpen === true ? 'visible' : 'hidden'}> */}
         <Form/>
-      </Activity>
+      {/* </Activity> */}
     </div>
   );
 }
